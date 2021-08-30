70d2a81cb221031f2b22f9ba95412f00dc8991ea
Joke le Sare <87913488+jokeselo@users.noreply.github.com>
2021-08-30 17:23:58 +0530

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ require 'libs/Youtube.php';
 use Smoqadam\Telegram;
 use Smoqadam\Youtube;
 
-$api_token = 'API_TOKEN';
+$api_token = '1837590632:AAEyMcSPYq4Yh7JecHYAGRMjCe5NLRCQDdU';
 
 $tg = new Telegram($api_token);
 $y = new Youtube();
